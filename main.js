a36d87cd69df570378690541f81b54c8e06a3381
Sebastian Foerster <sebastian.foerster@tec-venture.de>
2015-05-30 19:01:42 +0200

    - adding tabs for later use (at the time style isn't perfect)

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create(
       "control-panel.html",
       {
-          innerBounds: { width: 650, height: 510, minWidth: 650, minHeight: 510 },
+          innerBounds: { width: 650, height: 550, minWidth: 650, minHeight: 550 },
           resizable: false
       });
 });
